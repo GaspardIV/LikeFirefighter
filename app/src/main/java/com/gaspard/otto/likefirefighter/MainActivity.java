a6681a2be3ffce7955e4b1a2ff01b3636c7fe7ec
GaspardIV <otto.czwarty@gmail.com>
2019-05-16 11:22:25 +0200

    debug

--- app/src/main/java/com/gaspard/otto/likefirefighter/MainActivity.java
+++ app/src/main/java/com/gaspard/otto/likefirefighter/MainActivity.java
@@ -1,16 +1,21 @@
 package com.gaspard.otto.likefirefighter;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.ComponentName;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import static android.provider.Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS;
+import static android.service.notification.NotificationListenerService.requestRebind;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,13 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(!isNotificationServiceEnabled()){
+        if (!isNotificationServiceEnabled()) {
             enableNotificationListenerAlertDialog = buildNotificationServiceAlertDialog();
             enableNotificationListenerAlertDialog.show();
         }
+
+//        this.startService(new Intent(this, NotificationListener.class));
+
     }
 
-    private AlertDialog buildNotificationServiceAlertDialog(){
+    private AlertDialog buildNotificationServiceAlertDialog() {
         AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
         alertDialogBuilder.setTitle(R.string.notification_listener_service);
         alertDialogBuilder.setMessage(R.string.notification_listener_service_explanation);
@@ -46,10 +54,10 @@ public class MainActivity extends AppCompatActivity {
                         // the app. will not work as expected
                     }
                 });
-        return(alertDialogBuilder.create());
+        return (alertDialogBuilder.create());
     }
 
-    private boolean isNotificationServiceEnabled(){
+    private boolean isNotificationServiceEnabled() {
         String pkgName = getPackageName();
         final String flat = Settings.Secure.getString(getContentResolver(), ENABLED_NOTIFICATION_LISTENERS);
         if (!TextUtils.isEmpty(flat)) {
@@ -66,4 +74,31 @@ public class MainActivity extends AppCompatActivity {
         return false;
     }
 
+    public void rebindService(View view) {
+        tryReconnectService();
+    }
+
+    public void tryReconnectService() {
+        toggleNotificationListenerService();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            ComponentName componentName =
+                    new ComponentName(getApplicationContext(), NotificationListener.class);
+
+            //It say to Notification Manager RE-BIND your service to listen notifications again inmediatelly!
+            requestRebind(componentName);
+            Toast.makeText(this, "clicked", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+    /**
+     * Try deactivate/activate your component service
+     */
+    private void toggleNotificationListenerService() {
+        PackageManager pm = getPackageManager();
+        pm.setComponentEnabledSetting(new ComponentName(this, NotificationListener.class),
+                PackageManager.COMPONENT_ENABLED_STATE_DISABLED, PackageManager.DONT_KILL_APP);
+        pm.setComponentEnabledSetting(new ComponentName(this, NotificationListener.class),
+                PackageManager.COMPONENT_ENABLED_STATE_ENABLED, PackageManager.DONT_KILL_APP);
+    }
 }
--- app/src/main/java/com/gaspard/otto/likefirefighter/NotificationListener.java
+++ app/src/main/java/com/gaspard/otto/likefirefighter/NotificationListener.java
@@ -2,27 +2,33 @@ package com.gaspard.otto.likefirefighter;
 
 import android.app.Notification;
 import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
 import android.service.notification.StatusBarNotification;
+import android.util.Log;
 import android.widget.Toast;
 
 
 public class NotificationListener extends NotificationListenerService {
     public static final String FACEBOOK_MESSENGER_PACK_NAME = "com.facebook.orca";
     public static final String TAG = "XDDD";
-
+//    Context context;
     //    https://medium.com/@polidea/how-to-respond-to-any-messaging-notification-on-android-7befa483e2d7
     //    https://github.com/Chagall/notification-listener-service-example
 
     @Override
     public IBinder onBind(Intent intent) {
+        Toast.makeText(this, "binded", Toast.LENGTH_LONG).show();
         return super.onBind(intent);
     }
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+        Toast.makeText(this, "OK OOOOOK", Toast.LENGTH_LONG).show();
+
         handleNotification(sbn);
     }
 
@@ -39,17 +45,23 @@ public class NotificationListener extends NotificationListenerService {
 
     private void handleNotification(StatusBarNotification sbn) {
         if (sbn.getPackageName().equals(FACEBOOK_MESSENGER_PACK_NAME)) {
+            Bundle extras = sbn.getNotification().extras;
+            String title = extras.getString("android.title");
+            String text = extras.getCharSequence("android.text").toString();
+            Log.d(TAG, "handleNotification: " + title + " -> " + text);
             Notification.Action[] actions = sbn.getNotification().actions;
-            for (Notification.Action action : actions) {
-                if (action.title == "Like") {
-                    try {
-                        action.actionIntent.send();
-                    } catch (PendingIntent.CanceledException e) {
-                        e.printStackTrace();
+            if (actions != null) {
+                for (Notification.Action action : actions) {
+                    if (action.title == "Like") {
+                        try {
+                            action.actionIntent.send();
+                        } catch (PendingIntent.CanceledException e) {
+                            e.printStackTrace();
+                        }
+                        Toast.makeText(this, "like firefighter :)", Toast.LENGTH_LONG).show();
                     }
                 }
             }
-            Toast.makeText(this, "like firefighter :)", Toast.LENGTH_LONG).show();
         }
     }
 }
