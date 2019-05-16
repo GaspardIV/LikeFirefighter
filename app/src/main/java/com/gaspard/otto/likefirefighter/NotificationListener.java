865872542a5c08164fb8e345faa146d0e7727d7d
GaspardIV <otto.czwarty@gmail.com>
2019-05-16 05:13:44 +0200

    working likes

--- app/src/main/java/com/gaspard/otto/likefirefighter/NotificationListener.java
+++ app/src/main/java/com/gaspard/otto/likefirefighter/NotificationListener.java
@@ -1,5 +1,7 @@
 package com.gaspard.otto.likefirefighter;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.IBinder;
 import android.service.notification.NotificationListenerService;
@@ -9,7 +11,10 @@ import android.widget.Toast;
 
 public class NotificationListener extends NotificationListenerService {
     public static final String FACEBOOK_MESSENGER_PACK_NAME = "com.facebook.orca";
+    public static final String TAG = "XDDD";
 
+    //    https://medium.com/@polidea/how-to-respond-to-any-messaging-notification-on-android-7befa483e2d7
+    //    https://github.com/Chagall/notification-listener-service-example
 
     @Override
     public IBinder onBind(Intent intent) {
@@ -18,21 +23,38 @@ public class NotificationListener extends NotificationListenerService {
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
-        if (sbn.getPackageName().equals(FACEBOOK_MESSENGER_PACK_NAME)) {
-            Toast.makeText(this, "wez", Toast.LENGTH_LONG).show();
-        }
+        handleNotification(sbn);
     }
 
     @Override
     public void onNotificationRemoved(StatusBarNotification sbn) {
-//        int notificationCode = matchNotificationCode(sbn);
         if (sbn.getPackageName().equals(FACEBOOK_MESSENGER_PACK_NAME)) {
-            Toast.makeText(this, "wez", Toast.LENGTH_LONG).show();
+//            Toast.makeText(this, "wez", Toast.LENGTH_LONG).show();
             for (StatusBarNotification notification : this.getActiveNotifications()) {
                 if (notification.getPackageName().equals(FACEBOOK_MESSENGER_PACK_NAME)) {
-                    Toast.makeText(this, "jeszcze", Toast.LENGTH_LONG).show();
+//                    Toast.makeText(this, "jeszcze", Toast.LENGTH_LONG).show();
+                    handleNotification(notification);
                 }
             }
+        }
+    }
+
+    private void handleNotification(StatusBarNotification sbn) {
+        if (sbn.getPackageName().equals(FACEBOOK_MESSENGER_PACK_NAME)) {
+            Notification.Action[] actions = sbn.getNotification().actions;
+            try {
+                for (Notification.Action action : actions) {
+                    if (action.title == "Like") {
+                        action.actionIntent.send();
+                    }
+//                    Log.d(TAG, "onNotificationPosted: " + action.toString() + "   " + action.title);
+                }
+//                actions[0].actionIntent.send();
+            } catch (PendingIntent.CanceledException e) {
+                e.printStackTrace();
+            }
+//            Log.d("XDD", "onNotificationPosted: " + sbn.getNotification().toString());
+            Toast.makeText(this, "like firefighter :)", Toast.LENGTH_LONG).show();
         }
     }
 }
